f798f4ecbb86a172fd8c2ef9d304bddb77931d7b
Sami <sami@MacBook-Pro.hitronhub.home>
2022-03-11 11:51:12 -0800

    fc

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=225)
+    author = models.CharField(max_length=225)
+    slug = models.SlugField(max_length=225)
+
+    def __str__(self):
+        return self.title
