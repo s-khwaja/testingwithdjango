612a1b6553d8ca82ba752da0bcd7ea72decd0a96
Sami <sami@MacBook-Pro.hitronhub.home>
2022-03-11 12:11:50 -0800

    more edit

--- blog/tests.py
+++ blog/tests.py
@@ -5,7 +5,7 @@ from . models import Post
 class ModelTesting(TestCase):
 
     def setUp(self):
-        self.blog = Post.objects.create(name='django', author='django', slug='django')
+        self.blog = Post.objects.create(title='django', author='django', slug='django')
 
     def test_post_model(self):
         d = self.blog
